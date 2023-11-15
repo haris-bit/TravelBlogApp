8b0c2db2d599432b69e06a4ef1d6c05e1f3d7b9a
haris-bit <bsef19m528@pucit.edu.pk>
2023-11-15 11:58:15 +0500

    fix for route.js

--- travel-blog/app/api/auth/[...nextauth]/route.js
+++ travel-blog/app/api/auth/[...nextauth]/route.js
@@ -15,38 +15,40 @@ const handler = NextAuth({
     }),
   ],
 
-  // getting data about an existing running session
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
+  callbacks: {
+    // getting data about an existing running session
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
 
-    session.user.id = sessionUser._id.toString();
-    return session;
-  },
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
 
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
 
-      // check if the user already exists
-      const userExists = await User.findOne({ email: profile.email });
+        // check if the user already exists
+        const userExists = await User.findOne({ email: profile.email });
 
-      // if not, create a new user
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
+        // if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+
+        return false;
       }
-
-      return true;
-    } catch (error) {
-      console.log(error);
-
-      return false;
-    }
+    },
   },
 });
 
